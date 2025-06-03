fb263e72d4e36c0774c8af4a336fb26b753d52d6
AkBoAl <aksenov,boris8595@gmail.com>
2025-06-03 13:19:50 +0500

    Урок7 Задача4

--- src/main/java/RoadToWork/lesson7Task4.java
+++ src/main/java/RoadToWork/lesson7Task4.java
@@ -2,13 +2,13 @@ package RoadToWork;
 
 public class lesson7Task4 {
     public static void main(String[] args) {
-        System.out.println(calculate(15) + "   xxp" );
+        System.out.println(calculate(15));
     }
 
-    public static double calculate(double n) {
-        return buildExpression(n, (int)n);
+    public static double calculate(int n) {
+        return buildExpression(n, n);
     }
-    public static double buildExpression(double x, int step) {
-        return step == 0 ? x: buildExpression(step-1 + Math.sqrt(x), step-1);
+    public static double buildExpression(double n, int step) {
+        return step == 0 ? n: buildExpression(step-1 + Math.sqrt(n), step-1);
     }
 }
